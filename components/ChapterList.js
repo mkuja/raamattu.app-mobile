98042c9cf8c9d812044c818a63f9488b1e73c3e2
Mikko Kujala <m.kujala@live.com>
2024-10-09 19:50:38 +0300

    chapter list is in three columns now.

--- components/ChapterList.js
+++ components/ChapterList.js
@@ -1,8 +1,9 @@
-import {ActivityIndicator, FlatList, StyleSheet, Text, TouchableHighlight, View} from "react-native";
+import {ActivityIndicator, FlatList, ScrollView, StyleSheet, Text, TouchableHighlight, View} from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import {useContext, useEffect} from "react";
 import {NavigationContext} from "../Context/NavigationContext";
 import {useBibleBook} from "../hooks/useBible";
+import TopBar from "./TopBar";
 
 const ChapterList = () => {
     const navCtx = useContext(NavigationContext)
@@ -12,37 +13,43 @@ const ChapterList = () => {
     const renderItem = ({item, index, separators}) => {
         return (
             <TouchableHighlight
-                key={index+1}
+                key={index + 1}
                 onPress={() => {
-                    navCtx.setSelectedChapter(index+1)
+                    navCtx.setSelectedChapter(index + 1)
                     navigator.navigate("Luku ֍ Raamattu.app")
                 }}
                 onShowUnderlay={separators.highlight}
                 onHideUnderlay={separators.unhighlight}
+                style={style.item}
             >
 
-                <View style={style.items}>
-                    <Text style={style.items}>{index+1}</Text>
-                </View>
+                <Text style={{textAlign: "center"}}>{index + 1}</Text>
 
             </TouchableHighlight>
         )
     }
 
     return (
-        data && !loading ? <FlatList
-            data={Array.from({length: data.NumChapters}, (_, i) => i+1)}
-            renderItem={renderItem} /> : <ActivityIndicator size="large" />
+        data && !loading ? (
+            <>
+                <TopBar pageName={navCtx.selectedBook}/>
+                <FlatList numColumns={3}
+                          scrollEnabled={true} focusable={true}
+                          data={Array.from({length: data.NumChapters}, (_, i) => i + 1)}
+                          renderItem={renderItem}/>
+            </>
+        ) : <ActivityIndicator size="large"/>
     )
 }
 
 const style = StyleSheet.create({
-    items: {
+    item: {
+        padding: 8,
+        margin: 8,
         fontSize: 18,
-        paddingTop: 8,
-        paddingBottom: 8,
+        width: "30%",
         textAlign: "center"
-    }
+    },
 })
 
 export default ChapterList
